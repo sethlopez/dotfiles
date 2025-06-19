b8b78874c1953372bccc4746b04ba753c4365311
Seth Lopez <git@sethlopez.dev>
2025-06-18 21:49:37 -0400

    Add render-markdown plugin

---
+++ nvim/dot-config/nvim/lua/sethlopez/plugins/render-markdown.lua
@@ -0,0 +1,6 @@
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	opts = {},
+}
